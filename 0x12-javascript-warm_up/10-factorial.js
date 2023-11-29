a5694290cadd0293807425ae436e51a88964e157
iinhle <sawcyzee@gmail.com>
2023-11-29 08:38:17 -0800

    done

--- 0x12-javascript-warm_up/10-factorial.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const args = process.argv;
-function factorial (n) {
-  if (!n || n <= 1) {
-    return 1;
-  }
-  return n * factorial(n - 1);
-}
-console.log(factorial(parseInt(args[2])));
--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-const args = process.argv;
-if (!args[2] || !parseInt(args[2])) {
-  console.log('Not a number');
-} else {
-  console.log('My number: ' + parseInt(args[2]));
-}
